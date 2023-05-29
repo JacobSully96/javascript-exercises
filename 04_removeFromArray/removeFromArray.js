f4732a54871a05f2b4f91afa9f7c0ededef506e5
Jacob Sullivan <72237015+JacobSully96@users.noreply.github.com>
2023-05-28 23:39:54 -0500

    04 RemoveFromArray first Test PASS

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, toRemove) {
+ 
+    newArray = [];
 
+    for(let i = 0; i < array.length - 1; i++){
+        if(array[i] === toRemove){
+        array.splice(i, 1);
+        }
+        newArray[i] = array[i];
+    }
+
+    return newArray;
+  
 };
 
 // Do not edit below this line
