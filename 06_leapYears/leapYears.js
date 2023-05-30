37812d6dfa06a54ac3e30d879f63e3e02b03f409
Jacob Sullivan <72237015+JacobSully96@users.noreply.github.com>
2023-05-30 16:35:09 -0500

    06_leapYears Complete

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    if(year % 400 === 0){
+        return true;
+    }else if(year % 100 === 0){
+        return false;
+    }else if(year % 4 === 0){
+        return true;
+    }else{
+        return false;
+    }
 
 };
+
+// Divisible by 4 Leap Year
+// Divisible by 100 NOT Leap year ENLESS divisible by 400
+
 
 // Do not edit below this line
 module.exports = leapYears;
