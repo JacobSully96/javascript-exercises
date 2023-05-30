15c7100ffbc6649a78f735e7dc451cafd8509463
Jacob Sullivan <72237015+JacobSully96@users.noreply.github.com>
2023-05-30 16:52:31 -0500

    07 tempConversions Complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+
+  let temp = Math.round(((f - 32) * 5/9) * 10) / 10;
+  return temp;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+
+  let temp = Math.round((c * 9/5 + 32) * 10) / 10;
+  return temp;
+
 };
+
+
+
+// F = (C) * 9/5 + 32
+// C = (F - 32) * 5/9
+// the additional * 10 then / 10 is to get the single decimal place for some cases
+
 
 // Do not edit below this line
 module.exports = {
